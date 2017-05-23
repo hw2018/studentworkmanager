f824950a1813ead0bcb3ce0424cdfe13f11fd062
huangwei <534274294@qq.com>
2017-05-23 22:41:13 +0800

    清空

--- StudentWorkManager/src/unp/student/work/manager/dao/CategoryDao.java
+++
@@ -1,8 +0,0 @@
-package unp.student.work.manager.dao;
-
-import unp.student.work.manager.model.Category;
-
-public interface CategoryDao {
-	public void save(Category category);
-
-}
--- StudentWorkManager/src/unp/student/work/manager/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package unp.student.work.manager.service;
-
-import unp.student.work.manager.model.Category;
-
-public interface CategoryService {
-
-	
-	public void save(Category category); //用来测试Hibernate环境  
-}
